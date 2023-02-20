6c81133e39f568e3dcac329fee18e987fe60943c
xcodeplanb <chobo82kr@gmail.com>
2023-02-20 18:10:35 +0900

    plugin test

--- app/src/main/java/com/example/structure/Constants.kt
+++ app/src/main/java/com/example/structure/Constants.kt
@@ -6,4 +6,3 @@ const val GITHUB_TOKEN = "token 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"
 const val ICON_URL = "https://openweathermap.org/img/wn/"
 const val APP_ID = "4a3adc67e9283ea7586bad273e9dcd6d"
 const val KAKAO_DAUM_SEARCH_IMAGE = "https://dapi.kakao.com/v2/search/image"
-
