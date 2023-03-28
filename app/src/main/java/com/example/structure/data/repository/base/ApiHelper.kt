d59c4236243f566d0a4a9ccafbc62dd85e27fe3d
xcodeplanb <chobo82kr@gmail.com>
2023-03-28 20:58:39 +0900

    add Dispatchers in safeApiCall

--- app/src/main/java/com/example/structure/data/repository/base/ApiHelper.kt
+++ app/src/main/java/com/example/structure/data/repository/base/ApiHelper.kt
@@ -1,18 +1,22 @@
 package com.example.structure.data.repository.base
 
 import com.example.structure.api.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 
 suspend fun <T> safeApiCall(apiCall: suspend () -> T ): Resource<T> {
-    return try {
-        Resource.Success(apiCall.invoke())
-    } catch (throwable: Throwable) {
-        when (throwable) {
-            is HttpException -> {
-                Resource.Failure(throwable.code(), throwable.response()?.errorBody())
-            }
-            else -> {
-                Resource.Failure(null, null)
+    return withContext(Dispatchers.IO)  {
+        try {
+            Resource.Success(apiCall.invoke())
+        } catch (throwable: Throwable) {
+            when (throwable) {
+                is HttpException -> {
+                    Resource.Failure(throwable.code(), throwable.response()?.errorBody())
+                }
+                else -> {
+                    Resource.Failure(null, null)
+                }
             }
         }
     }
